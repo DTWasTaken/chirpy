2ebb9ec14673b21c892e70bc4d0fe58cc0e87e0c
Daniel Terry <daniel@danielterry.org>
2026-09-02 16:47:45 -0400

    CH1L9

--- main.go
+++ main.go
@@ -7,18 +7,30 @@ import (
 
 func main() {
 	// Create a ServeMux for routing requests
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
 	
 	// Add path handlers
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
-	serveMux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+	mux.Handle(
+		"/app/",
+		http.StripPrefix(
+			"/app/",
+			http.FileServer(http.Dir(".")),
+		),
+	)
+	mux.HandleFunc("/healthz", handlerHealthz)
 	
 	// Create a new http Server
 	s := &http.Server{
 		Addr:		":8080",
-		Handler:	serveMux,
+		Handler:	mux,
 	}
 	
 	// Start the server
 	log.Fatal(s.ListenAndServe())
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
